ed8298dc987ef390ed9f88ab4658651fd81905b7
Donielle20 <donielle.hope@yahoo.com>
2023-04-03 21:48:27 -0500

    lab 5 Progress

--- app/models.py
+++ app/models.py
@@ -1,1 +1,41 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+from datetime import datetime
+
+class Movies(db.Model):
+    # You can use this to change the table name. The default convention is to use
+    # the class name. In this case a class name of UserProfile would create a
+    # user_profile (singular) table, but if we specify __tablename__ we can change it
+    # to `user_profiles` (plural) or some other name.
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(80))
+    description = db.Column(db.Text)
+    poster = db.Column(db.String(80))
+    created_at = db.Column(db.DateTime())
+
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.poster = poster
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
